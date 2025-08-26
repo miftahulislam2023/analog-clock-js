85cdbd2ae18ec8342735c9e751b2594314f23b4d
Miftahul Islam <138649177+miftahulislam2023@users.noreply.github.com>
2025-08-26 16:49:54 +0600

    Add: functions to convert

--- script.js
+++ script.js
@@ -10,22 +10,45 @@ const centerY = canvas.getAttribute("width") / 2;
 const radius = 150;
 
 //draw circle
-ctx.beginPath();
-ctx.strokeStyle = 'black';
-ctx.lineWidth = 1;
-ctx.arc(centerX, centerY, radius, 0, Math.PI * 2);
-ctx.stroke();
+function drawCircle(x, y, r, width, color) {
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = width;
+    ctx.arc(x, y, r, 0, Math.PI * 2);
+    ctx.stroke();
+}
+
+// drawCircle(centerX, centerY, radius, 1, "black");
+drawCircle(0, 100, 10, 1, "black");
+
+//function to convert from Polar to Cartesian Coordinate
+
+function polarToCartesian(r, theta) {
+    // console.log(theta);
+    // console.log(Math.sin(theta));
+    // console.log(Math.cos(theta));
+    const x = r * Math.sin(theta);
+    const y = r * Math.cos(theta);
+    return { x, y };
+}
 
 //draw second-hand
 function drawSecondHand(deg = 0) {
     const secondHandLength = radius * 0.8;
+    let { x, y } = polarToCartesian(secondHandLength, deg * (Math.PI / 180));
+    console.log(x, y);
+    x = x + centerX;
+    y = - y + centerY;
+    console.log(x, y);
     ctx.beginPath();
     ctx.moveTo(centerX, centerY);
     ctx.lineWidth = 1;
     ctx.strokeStyle = 'blue';
-    ctx.lineTo(centerX, (centerY - secondHandLength));
+    ctx.lineTo(x, y);
     ctx.stroke();
 }
+
+drawSecondHand(0);
 
 //draw minute-hand
 function drawMinuteHand() {
@@ -51,10 +74,8 @@ function drawHourHand() {
 
 //animate second-hand
 // let start;
-// drawSecondHand();
 // function animateSecond(timeStamp) {
 
 //     requestAnimationFrame(animateSecond);
 // }
-
 // requestAnimationFrame(animateSecond);
